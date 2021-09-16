1b704628e2f1e3cf6891685c34eac8bea9497e1e
Geofed <theglassofdawn@gmail.com>
2021-09-16 15:08:25 +0100

    small changes

--- .idea/src/amex/eanstis/FizzBuzz.java
+++ .idea/src/amex/eanstis/FizzBuzz.java
@@ -1,17 +1,17 @@
 package amex.eanstis;
 
-public class fizzBuzz {
+public class FizzBuzz {
 
     public static void main(String[] args) {
 
-        for (int i = 1;i <= 100;i++) {
+        for (int i = 1; i <= 100; i++) {
             String output = "";
 
             if (i % 3 == 0)
                 output = output + "Fizz";
             if (i % 5 == 0)
                 output = output + "Buzz";
-            if (output == "")
+            if ("".equals(output))
                 output = output + i;
 
             System.out.println(output);
