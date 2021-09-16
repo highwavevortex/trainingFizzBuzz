8e96580c0dadb7e88c3fdd27517f5a6503316a3f
Geofed <theglassofdawn@gmail.com>
2021-09-16 14:40:41 +0100

    added fizzbuzz

--- .idea/src/amex/eanstis/fizzBuzz.java
+++ .idea/src/amex/eanstis/fizzBuzz.java
@@ -4,16 +4,17 @@ public class fizzBuzz {
 
     public static void main(String[] args) {
 
-        for (int i = 1;i <= 30;i++) {
-            System.out.println(i);
+        for (int i = 1;i <= 100;i++) {
+            String output = "";
 
-            if (i % 3 == 0) {
-                System.out.println("Fizz");
-            }
+            if (i % 3 == 0)
+                output = output + "Fizz";
+            if (i % 5 == 0)
+                output = output + "Buzz";
+            if (output == "")
+                output = output + i;
 
-            else if (i % 5 == 0) {
-                System.out.println("Buzz");
-            }
+            System.out.println(output);
         }
 
     }
